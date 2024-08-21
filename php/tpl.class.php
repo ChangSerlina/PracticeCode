536c4f2988f9ba36f2f16625ecb97e4923c7dd21
榕軒 <serlina0504@gmail.com>
2024-08-22 01:10:27 +0800

    [#4] 優化渲染模板的程式

--- php/tpl.class.php
+++ php/tpl.class.php
@@ -1,20 +1,24 @@
 <?php
 namespace tpl;
-/**
-* Class Tpl
-*/
-class Tpl {
-protected $view_dir;//模板文件
-protected $cache_dir;//缓存文件
-protected $lifetime;//过期时间
-protected $vars = [];//存放显示变量的数组
-/**
-* Tpl constructor.
-* @param string $view_dir
-* @param string $cache_dir
-* @param string $lifetime
-*/
-public function __construct($view_dir='', $cache_dir='', $lifetime=''){
 
-}
+class Tpl
+{
+    protected $tpl;
+
+    public function __construct($templateFile = '')
+    {
+        $this->tpl = $templateFile;
+    }
+
+    public function render($template, $variables = null)
+    {
+        $output = file_get_contents($this->tpl . $template);
+        if ($variables != null) {
+            foreach ($variables as $key => $value) {
+                $output = str_replace('{{' . $key . '}}', $value, $output);
+            };
+            return $output;
+        }
+        return $output;
+    }
 }
